fc3c7ea5cdf4b51f5f84ccae61bbed633710332f
Jonathan Edgecombe <jonathanedgecombe@gmail.com>
2017-07-23 21:41:21 +0100

    Add login detail filling after previous logout.

--- src/main/java/com/mithrilclient/module/LoginDetailsModule.java
+++ src/main/java/com/mithrilclient/module/LoginDetailsModule.java
@@ -4,6 +4,19 @@ import com.mithrilclient.config.ConfigManager;
 import com.mithrilclient.reflection.ReflectionHooks;
 
 public final class LoginDetailsModule extends Module {
+	private int lastLoginState = 0;
+
+	@Override
+	public void tick() {
+		int loginState = ReflectionHooks.getLoginState();
+
+		if (loginState == 2 && lastLoginState == 0) {
+			init();
+		}
+
+		lastLoginState = loginState;
+	}
+
 	@Override
 	public void init() {
 		String username = ConfigManager.CONFIG.getDefaultUsername();
